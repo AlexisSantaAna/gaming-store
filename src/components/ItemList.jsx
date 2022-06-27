c7428da7486ee16b485aab75e6b0098960959342
Alexis Santa Ana <ibanez2112@hotmail.com>
2022-06-27 00:48:34 -0300

    probando atropos 2

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -6,7 +6,7 @@ const ItemList = ({ products }) => {
     return (
         <>
             <h2 className="my-5 text-center main-title animate__animated animate__bounceInLeft">Listado de productos</h2>
-            <div className="row row-cols row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-4 mb-5 animate__animated animate__zoomInUp">
+            <div className="mobile-width row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-4 mb-5 animate__animated animate__zoomInUp">
                 {products?.map(product => <Item key={product.id} product={product} />)}
             </div>
         </>
